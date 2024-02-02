85675826df2bb2e4dca1ca456c130d5fb0f17c27
cyoq <di9akoz@gmail.com>
2024-02-02 20:46:33 +0200

    Use array instead of hardcoded image

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod oodle;
 
 fn main() {
     let oodle = Oodle::new("resources/oo2core_8_win64.dll");
-    let data = std::fs::read("./lenna.png").unwrap();
+    let data = (0..100).collect::<Vec<_>>();
 
     let dst = oodle.compress(&data, oodle::Compressor::Kraken, oodle::Level::Normal);
 
