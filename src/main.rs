6c65e0b788f5e78a2cd1ac383844221b31d28cd9
cyoq <di9akoz@gmail.com>
2024-02-02 20:08:18 +0200

    Apply cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,16 @@ use std::vec;
 mod oodle;
 
 fn main() {
-  let oodle = oodle::Oodle::new("resources/oo2core_8_win64.dll");
-  let data = std::fs::read("./lenna.bmp").unwrap();
+    let oodle = oodle::Oodle::new("resources/oo2core_8_win64.dll");
+    let data = std::fs::read("./lenna.png").unwrap();
 
-  let mut dst = vec![];
-  oodle.compress(&data, &mut dst, oodle::Compressor::Kraken, oodle::Level::Normal);
+    let mut dst = vec![];
+    oodle.compress(
+        &data,
+        &mut dst,
+        oodle::Compressor::Kraken,
+        oodle::Level::Normal,
+    );
 
-  println!("src_len {} dst_len {}", data.len(), dst.len());
+    println!("src_len {} dst_len {}", data.len(), dst.len());
 }
