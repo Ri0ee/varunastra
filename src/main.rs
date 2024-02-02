cf3b809a3da65f537dabb40311c66a3b1fb8efbb
cyoq <di9akoz@gmail.com>
2024-02-02 20:15:58 +0200

    Use crate import

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use crate::oodle::Oodle;
+
 mod oodle;
 
 fn main() {
-    let oodle = oodle::Oodle::new("resources/oo2core_8_win64.dll");
+    let oodle = Oodle::new("resources/oo2core_8_win64.dll");
     let data = std::fs::read("./lenna.png").unwrap();
 
     let dst = oodle.compress(&data, oodle::Compressor::Kraken, oodle::Level::Normal);
